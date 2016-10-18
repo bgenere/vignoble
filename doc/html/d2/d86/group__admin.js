48f7ed6ae90ef2e322f61bf77468e4150093d054
bgenere <bgenere@gmail.com>
2016-10-18 19:11:53 +0200

    doxygen doc update

--- doc/html/d2/d86/group__admin.js
+++ doc/html/d2/d86/group__admin.js
@@ -1,5 +1,6 @@
 var group__admin =
 [
     [ "module_about.php", "d5/da0/module__about_8php.html", null ],
+    [ "plot_extrafields.php", "d3/dd2/plot__extrafields_8php.html", null ],
     [ "admin.html.lib.php", "dd/dac/admin_8html_8lib_8php.html", null ]
 ];
--- doc/html/db/dfe/group__plot.js
+++ doc/html/db/dfe/group__plot.js
@@ -3,6 +3,5 @@ var group__plot =
     [ "plot_card.php", "df/d3c/plot__card_8php.html", null ],
     [ "plot_info.php", "dd/d24/plot__info_8php.html", null ],
     [ "plot_list.php", "da/d01/plot__list_8php.html", null ],
-    [ "plot_notes.php", "d3/d16/plot__notes_8php.html", null ],
-    [ "plot_extrafields.php", "d3/dd2/plot__extrafields_8php.html", null ]
+    [ "plot_notes.php", "d3/d16/plot__notes_8php.html", null ]
 ];
