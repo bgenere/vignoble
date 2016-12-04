50cc85c03833b6dd9fbd39e201697ce278ff007f
bgenere <bgenere@gmail.com>
2016-12-04 18:48:49 +0100

    Vignoble area using two column template for boxes

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-/* Copyright (C) 2016 Bruno Généré      <bgenere@webiseasy.org>
+/*
+ * Copyright (C) 2016 Bruno Généré <bgenere@webiseasy.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -8,27 +9,24 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/** 
+/**
  * \file index.php
- * \brief Main page - module dashboard
- *  
- *  Displays the Vignoble dashboard
- *  
- *  \ingroup dashboard
+ * \brief Main page - Displays the Vignoble dashboard
+ *
+ * \ingroup dashboard
  */
-
 @include './tpl/maindolibarr.inc.php';
 
-/*  get dashboard boxes */
-dol_include_once('/vignoble/core/boxes/plotsummarybox.php');
-	
+/* get dashboard boxes */
+dol_include_once('/vignoble/core/boxes/plotslastchanged.php');
+
 /* get language files */
 $langs->load("vignoble@vignoble");
 
@@ -42,14 +40,24 @@ $db->close();
  */
 function displayView()
 {
-	global $db,$conf,$langs,$user;
+	global $db, $conf, $langs, $user;
 	
-	llxHeader('', $langs->trans('Dashboard'));
+	llxHeader('', $langs->trans('VineYardArea'));
+	print load_fiche_titre($langs->trans("VineYardArea"), '', 'object_vignoble@vignoble');
 	
-	print('<h1> Dashboard </h1>');
-	$box=new plotsummarybox($db);
-	$box->loadBox(10);
-	$box->showBox($box->info_box_head, $box->info_box_contents);
+	$plotslastchanged = new plotslastchanged($db);
+	$plotslastchanged->loadBox(10);
+	// $box->showBox($box->info_box_head, $box->info_box_contents);
+	/*
+	 * Show boxes
+	 */
+	print '<div class="fichecenter"><div class="fichethirdleft">';
+	$plotslastchanged->showBox($plotslastchanged->info_box_head, $plotslastchanged->info_box_contents);
+	$plotslastchanged->showBox($plotslastchanged->info_box_head, $plotslastchanged->info_box_contents);
+	print '</div><div class="fichetwothirdright"><div class="ficheaddleft">';
+	$plotslastchanged->showBox($plotslastchanged->info_box_head, $plotslastchanged->info_box_contents);
+	$plotslastchanged->showBox($plotslastchanged->info_box_head, $plotslastchanged->info_box_contents);
+	print '</div></div>';
 	
 	llxFooter();
 }
