67cee3e991b7ab28b9b622f44829e5deaf993663
bgenere <bgenere@gmail.com>
2016-12-08 15:01:56 +0100

    NEW added dummy cultivation menu.

--- core/modules/modVignoble.class.php
+++ core/modules/modVignoble.class.php
@@ -370,7 +370,7 @@ class modVignoble extends DolibarrModules
 			'perms' => '1'
 		);
 		/**
-		 * Define plot left menu
+		 * - Define plot left menu
 		 */
 		$this->menu[] = array(
 			'type' => 'left',
@@ -387,7 +387,7 @@ class modVignoble extends DolibarrModules
 			'perms' => '1'
 		);
 		/**
-		 * Define plot left menu entry New plot
+		 * 	- Define plot left menu entry New plot
 		 */
 		$this->menu[] = array(
 			'type' => 'left',
@@ -404,7 +404,7 @@ class modVignoble extends DolibarrModules
 			'perms' => '1'
 		);
 		/**
-		 * Define plot left menu entry Plot List
+		 * 	- Define plot left menu entry Plot List
 		 */
 		$this->menu[] = array(
 			'type' => 'left',
@@ -414,6 +414,57 @@ class modVignoble extends DolibarrModules
 			'titre' => 'List',
 			'langs' => 'vignoble@vignoble',
 			'url' => '/vignoble/plot_list.php',
+			'target' => '',
+			'position' => 10,
+			'user' => 2,
+			'enabled' => '$conf->vignoble->enabled',
+			'perms' => '1'
+		);
+		/**
+		 * - Define cultivation left menu
+		 */
+		$this->menu[] = array(
+			'type' => 'left',
+			'mainmenu' => 'vignoble',
+			'leftmenu' => 'cultivation',
+			'fk_menu' => 'fk_mainmenu=vignoble',
+			'titre' => 'Cultivation',
+			'langs' => 'vignoble@vignoble',
+			'position' => 2,
+			'url' => '/vignoble/index.php',
+			'target' => '',
+			'user' => 2,
+			'enabled' => '$conf->vignoble->enabled',
+			'perms' => '1'
+		);
+		/**
+		 *  - Define cultivation left menu entry New task
+		 */
+		$this->menu[] = array(
+			'type' => 'left',
+			'mainmenu' => 'vignoble',
+			'leftmenu' => 'cultivationTask_create',
+			'fk_menu' => 'fk_mainmenu=vignoble,fk_leftmenu=cultivation',
+			'titre' => 'NewTask',
+			'langs' => 'vignoble@vignoble',
+			'position' => 20,
+			'url' => '/vignoble/index.php?action=create',
+			'target' => '',
+			'user' => 2,
+			'enabled' => '$conf->vignoble->enabled',
+			'perms' => '1'
+		);
+		/**
+		 * Define plot left menu entry Plot List
+		 */
+		$this->menu[] = array(
+			'type' => 'left',
+			'mainmenu' => 'vignoble',
+			'leftmenu' => 'cultivationTask_list',
+			'fk_menu' => 'fk_mainmenu=vignoble,fk_leftmenu=cultivation',
+			'titre' => 'List',
+			'langs' => 'vignoble@vignoble',
+			'url' => '/vignoble/index.php',
 			'target' => '',
 			'position' => 10,
 			'user' => 2,
